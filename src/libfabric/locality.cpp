860e169651261690781ad5a895c55f9b088fc36b
John Biddiscombe <biddisco@cscs.ch>
2025-07-02 17:00:38 +0200

    clang-format-18 and remove unused includes in libfabric backend

--- src/libfabric/locality.cpp
+++ src/libfabric/locality.cpp
@@ -10,26 +10,20 @@
 
 #include <locality.hpp>
 
-namespace oomph
-{
-namespace libfabric
-{
+namespace oomph {
+namespace libfabric {
 
 // ------------------------------------------------------------------
 // format as ip address, port, libfabric address
 // ------------------------------------------------------------------
-iplocality::iplocality(const locality& l)
-: data(l)
-{
-}
+iplocality::iplocality(const locality &l) : data(l) {}
 
-std::ostream&
-operator<<(std::ostream& os, const iplocality& p)
-{
-    os << std::dec << NS_DEBUG::ipaddr(p.data.fabric_data()) << " - "
-       << NS_DEBUG::ipaddr(p.data.ip_address()) << ":" << NS_DEBUG::dec<>(p.data.port()) << " ("
-       << NS_DEBUG::dec<>(p.data.fi_address()) << ") ";
-    return os;
+std::ostream &operator<<(std::ostream &os, const iplocality &p) {
+  os << std::dec << NS_DEBUG::ipaddr(p.data.fabric_data()) << " - "
+     << NS_DEBUG::ipaddr(p.data.ip_address()) << ":"
+     << NS_DEBUG::dec<>(p.data.port()) << " ("
+     << NS_DEBUG::dec<>(p.data.fi_address()) << ") ";
+  return os;
 }
 
 } // namespace libfabric
